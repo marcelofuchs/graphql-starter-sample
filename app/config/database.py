f8b45b7a5bf0308899836a0d5c81ff738b9da200
Marcelo Fuchs <marcelof29@gmail.com>
2019-06-27 17:00:59 -0300

    testes com docker e python

--- app/config/database.py
+++ app/config/database.py
@@ -13,13 +13,13 @@ class Database(object):
 class DevelopmentConfig(Database):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://' + os.environ['DB_USERNAME'] + ':' + os.environ['DB_PASSWORD'] + '@' + \
-                              os.environ['DB_HOST'] + ":3306/" + os.environ['DB_DATABASE']
+                              os.environ['DB_HOST'] + ":33060/" + os.environ['DB_DATABASE']
 
 
 class ProductionConfig(Database):
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://' + os.environ['DB_USERNAME'] + ':' + os.environ['DB_PASSWORD'] + '@' + \
-                              os.environ['DB_HOST'] + ":3306/" + os.environ['DB_DATABASE']
+                              os.environ['DB_HOST'] + ":33060/" + os.environ['DB_DATABASE']
 
 
 def get_environment_database():
